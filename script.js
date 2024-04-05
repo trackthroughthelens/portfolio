00734b856771e52778e59bf34b633d4ee9b0cdd7
trackthroughthelens <166155645+trackthroughthelens@users.noreply.github.com>
2024-04-05 18:36:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-    const sections = document.querySelectorAll('.journey-section');
-    sections.forEach((section) => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-            document.body.style.backgroundColor = section.dataset.theme;
-        }
-    });
-});
